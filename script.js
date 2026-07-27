30c3df8c0e398d6cb2092aa924a809868dbb9f1c
deepakvet304-dot <deepakvet304@gmail.com>
2026-07-27 21:35:05 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// PetVet Home Service Nepal
 
+console.log("PetVet Home Service Nepal Loaded!");
+
+const form = document.querySelector("form");
+
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you! Your appointment request has been received. We will contact you soon.");
+    form.reset();
+  });
+}
