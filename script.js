3ac3a0a15515820b40c76c43cb7b190cf2bbff83
deepakvet304-dot <deepakvet304@gmail.com>
2026-07-30 13:00:41 +0545

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ function topFunction() {
     behavior: "smooth"
   });
 }
+const darkBtn = document.getElementById("darkModeBtn");
+
+darkBtn.addEventListener("click", function(){
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+darkBtn.innerHTML="☀ Light Mode";
+
+}else{
+
+darkBtn.innerHTML="🌙 Dark Mode";
+
+}
+
+});
