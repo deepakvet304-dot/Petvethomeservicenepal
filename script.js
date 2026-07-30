7edfcacd8bd59ac8e5ee5c8ea572eca8b3e31915
deepakvet304-dot <deepakvet304@gmail.com>
2026-07-30 12:51:48 +0545

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ if (form) {
     form.reset();
   });
 }
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
